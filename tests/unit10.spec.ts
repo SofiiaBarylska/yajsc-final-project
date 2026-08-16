9624810e3b5d5afbad963890702fd99ecaab3db1
Sofiia Barylska <barylska.sonyaaa@gmail.com>
2026-08-16 21:08:34 +0300

    Refactor login test to use data-test attributes for element selection

--- tests/unit10.spec.ts
+++ tests/unit10.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test('login to the site', async ({ page }) => {
   await page.goto('/auth/login');
 
-  await page.getByPlaceholder('Your email').fill('customer@practicesoftwaretesting.com');
-  await page.getByPlaceholder('Your password').fill('welcome01');
-  await page.getByRole('button', { name: 'Login' }).click();
-
+  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
+  await page.getByTestId('password').fill('welcome01');
+  await page.getByTestId('login-submit').click();
+  await page.waitForTimeout(4000);
 
    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
    await expect(page.getByRole('heading', { name: 'My account' })).toHaveText('My account');
