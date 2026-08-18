8d32dcab2644a8de63dc83c488a3b7fcfcfe1dda
Sofiia Barylska <barylska.sonyaaa@gmail.com>
2026-08-18 16:33:14 +0300

    Update .gitignore and clean up launch.json; refine login test expectations

--- tests/unit10.spec.ts
+++ tests/unit10.spec.ts
@@ -6,9 +6,8 @@ test('login to the site', async ({ page }) => {
   await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
   await page.getByTestId('password').fill('welcome01');
   await page.getByTestId('login-submit').click();
-  await page.waitForTimeout(4000);
 
-   await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+   await expect(page).toHaveURL('/account');;
    await expect(page.getByRole('heading', { name: 'My account' })).toHaveText('My account');
    await expect(page.locator('[data-test="nav-menu"]')).toHaveText('Jane Doe');
 });
