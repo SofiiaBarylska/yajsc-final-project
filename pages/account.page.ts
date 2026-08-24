4fcbb92817ec05811f329dc3eb852746dd9f099e
Sofiia Barylska <barylska.sonyaaa@gmail.com>
2026-08-24 21:58:29 +0300

    feat: enhance account page and login test with user data integration

--- pages/account.page.ts
+++ pages/account.page.ts
@@ -5,10 +5,12 @@ export class AccountPage {
   page: Page;
   headerFragment: HeaderFragments;
   pageTitle: Locator;
+  navMenu: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.headerFragment = new HeaderFragments(page);
     this.pageTitle = page.getByRole("heading", { name: "My account" });
+    this.navMenu = page.getByTestId("nav-menu");
   }
 };
